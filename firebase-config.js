88689f18e8c2be8e7b1bf7083d36c14544534bf3
santi85227-hue <santi85227@gmail.com>
2026-05-17 17:56:05 -0500

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCWxAs_JLDo0tX80wMe1rlHFx6hCU79DwQ",
+  authDomain: "montacargas-tecnicos.firebaseapp.com",
+  projectId: "montacargas-tecnicos",
+  storageBucket: "montacargas-tecnicos.firebasestorage.app",
+  messagingSenderId: "321697235731",
+  appId: "1:321697235731:web:d54b1bf957d3a95641d0e6"
+};
