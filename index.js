537e6506ab8541b25fb7b70ab8022fafd8e7fd9a
santi85227-hue <santi85227@gmail.com>
2026-05-19 20:55:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,8 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/estado", (req, res) => {
-    res.setHeader("Content-Type", "application/json");
-    res.send('{"Activo"}');
-});
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor iniciado en puerto", PORT);
+    console.log("Servidor iniciado");
 });
